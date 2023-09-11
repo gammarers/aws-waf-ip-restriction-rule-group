4ee7632b7f685e5f993e39612cac617db65157aa
yicr <yicr@users.noreply.github.com>
2023-09-11 12:19:10 +0900

    chore(deps)!: jsii version set ~5.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
   typescriptVersion: '4.9.x',
+  jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-waf-ip-restriction-rule-group',
   description: 'This is an AWS CDK Construct for IP Restriction Rule Group on WAF V2',
